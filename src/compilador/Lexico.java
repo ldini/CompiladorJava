8773be0075df6445385be3b6905c05703dda6ec6
Leandro <leandro.david.dini@gmail.com>
2022-11-01 23:53:13 -0300

    implementacion de las acciones semanticas

--- src/compilador/Lexico.java
+++ src/compilador/Lexico.java
@@ -6,12 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.function.Function;
 
 public class Lexico {
 	private ArrayList<Character> codigoFuente; // SE REPRESENTA EL CODIGO FUENTE COMO UN ARRAY DE CHARS.
 	private String buffer; // SE UTILIZA PARA IR FORMANDO EL IDENTIFICADOR.
 	private static int [][] MatrizTransicion;
+	private static int [][] MatrizAccionesSemanticas;
+	private static HashMap<String,Integer> TablaDeSimbolos;
 	private int index; // POSICION ACTUAL EN EL CODIGO FUENTE.
+	private ArrayList<Function> AccionesSemanticas;
 	
 	// TOTAL DE ESTADOS.
 	public static int ESTADO_INICIAL = 0;      //LETRAS MAYUSCULA	
@@ -41,20 +46,78 @@ public class Lexico {
 	public static int PUNTO = 20;				// .
 	public static int EXCLAMACION = 21;			// !
 	public static int COMILLA_SIMPLE = 22;		// '
-	public static int OTRO = 23;				//CUALQUIER OTRO CARACTER NO CONTEMPLADO EN ESTA ENTREGA
-	public static int EOF = 24;					//FIN DEL ARCHIVO
+	public static int ASTERISCO = 23;		// '	
+	public static int OTRO = 24;				//CUALQUIER OTRO CARACTER NO CONTEMPLADO EN ESTA ENTREGA
+	public static int EOF = 25;					//FIN DEL ARCHIVO
 	
-	public static int ENTRADAS = 25;			//TOTAL DE ENTRADAS POSIBLES.
+	public static int ENTRADAS = 26;			//TOTAL DE ENTRADAS POSIBLES.
 	public static int ESTADOS = 13;				//TOTAL DE ESTADOS DISPONIBLES.
 	
 	//CONSTRUCTOR LEXICO
 	public Lexico(String path) throws FileNotFoundException, IOException {
 		codigoFuente = getCodigoFuente(path);
 		MatrizTransicion = inicializarMatrizDeTransicion();
+		MatrizAccionesSemanticas = inicializarMatrizDeAccionesSemanticas();
+		cargarAccionesSemanticas();
+		TablaDeSimbolos = new HashMap<String,Integer>();
+		InicializarTablaDeSimbolos();
 		index = 0;
 	}
 	
 
+//	// SEPARA DEL CODIGO FUENTE UN TOKEN SIN MODIFICAR EL MISMO
+//	public int getToken() {
+//		Character simbolo = codigoFuente.get(index);
+//		int estadoActual = ESTADO_INICIAL;
+//		
+//		while(estadoActual != ESTADO_FINAL && !EOF()) {
+//			
+//			estadoActual = nextEstado(estadoActual,simbolo);
+//		}
+//		
+//		
+//		buffer = "";
+//		
+//		
+//		
+//		
+//		if(EOF())
+//			System.out.println("----FIN DE ARCHIVO----");
+//		else {
+//			
+//		
+//		
+//		//ESTE ITERACION ES UTILIZADA LIMPIAR EL IDENTIFICADOR
+//		while(estadoActual == ESTADO_INICIAL && !EOF()) {
+//			simbolo = codigoFuente.get(index);
+//			buffer += simbolo;
+//			estadoActual = nextEstado(estadoActual,simbolo);
+//			if(estadoActual == ESTADO_INICIAL) {
+//				buffer = "";
+//			}
+//			index ++;
+//		} 
+//		
+//		//SE RECONOCE EL IDENTIFICADOR
+//		while(estadoActual != ESTADO_FINAL && !EOF()) {
+//			simbolo = codigoFuente.get(index);
+//			estadoActual = nextEstado(estadoActual,simbolo);
+//			if(estadoActual != ESTADO_FINAL) {
+//				if(buffer.length() < 25)
+//					buffer += simbolo;
+//				index++;
+//			}
+//		}
+//		
+//
+//		}
+//		
+//		System.out.println(buffer);
+//		
+//		if(TablaDeSimbolos.containsKey(buffer)) 
+//			return TablaDeSimbolos.get(buffer);
+//		return -1;
+//	}
 	
 	// SEPARA DEL CODIGO FUENTE UN TOKEN SIN MODIFICAR EL MISMO
 	public int getToken() { 
@@ -66,7 +129,8 @@ public class Lexico {
 			System.out.println("----FIN DE ARCHIVO----");
 		else {
 		
-		while(estadoActual == ESTADO_INICIAL && !EOF()) {
+		//ESTE ITERACION ES UTILIZADA LIMPIAR EL IDENTIFICADOR
+		/*while(estadoActual == ESTADO_INICIAL && !EOF()) {
 			simbolo = codigoFuente.get(index);
 			buffer += simbolo;
 			estadoActual = nextEstado(estadoActual,simbolo);
@@ -74,23 +138,28 @@ public class Lexico {
 				buffer = "";
 			}
 			index ++;
-		} 
+		} */
 		
-
+		//SE RECONOCE EL IDENTIFICADOR
 		while(estadoActual != ESTADO_FINAL && !EOF()) {
 			simbolo = codigoFuente.get(index);
+			ejecutarAccionesSemanticas(estadoActual,simbolo);
 			estadoActual = nextEstado(estadoActual,simbolo);
-			if(estadoActual != ESTADO_FINAL) {
-				buffer += simbolo;
-				index++;
-			}
+			index++;
 		}
 		
 
 		}
 		
 		System.out.println(buffer);
-		return 1;
+		
+		if(TablaDeSimbolos.containsKey(buffer)) 
+			return TablaDeSimbolos.get(buffer);
+		return -1;
+	}
+	
+	private void ejecutarAccionesSemanticas(int estadoActual,char caracterActual) {
+		AccionesSemanticas.get(MatrizAccionesSemanticas[estadoActual][toInt(caracterActual)]).apply(1);
 	}
 	
 	// A PARTIR DE EL ESTADO ACTUAL Y UN CARACTER DETERMINA CUAL ES EL SIGUIENTE ESTADO
@@ -111,7 +180,7 @@ public class Lexico {
         return fuente;
 	}
 	
-	private boolean EOF() {
+	public boolean EOF() {
 		return index == codigoFuente.size();
 	}
 	
@@ -122,7 +191,7 @@ public class Lexico {
 		//DENTRO DE LA MATRIZ HAY ESTADOS DE ERROR QUE SE MANEJAN CON ACCIONES SEMANTICAS Y VAN A ESTADO FINAL.
 				
 		//LM-{F}	lm		Digito	Blanco	/n	Tab	F   _   =   <	>	(	)	{	}	,	;	-   + 	/	.	!	 ' 	
-			{1,		1,		7,		0,		0,	0,	1,	F,	5,	3,	4,	F,	F,	F,	F,	F,	F,	F,	F,	F,	8,	F,	10},	// ESTADO 0 INICIAL 
+			{1,		1,		7,		0,		0,	0,	1,	0,	5,	3,	4,	F,	F,	F,	F,	F,	F,	F,	F,	F,	8,	F,	10},	// ESTADO 0 INICIAL 
 			{1,		1,		1,		F,		F,	F,	1,	1,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F},		// ESTADO 1
 			{F,		F,		F,		F,		F,	F,	F,	F,	F,	5,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F},		// ESTADO 2
 			{F,		F,		F,		F,		F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F,	F},		// ESTADO 3
@@ -198,4 +267,190 @@ public class Lexico {
 
 		}
 	}
+	
+	private static void InicializarTablaDeSimbolos(){
+		
+//		//SIMBOLOS INDIVIDUALES
+//		TablaDeSimbolos.put("<",60);
+//		TablaDeSimbolos.put(">",62);
+//		TablaDeSimbolos.put("(",72);
+//		TablaDeSimbolos.put(")",73);
+//		TablaDeSimbolos.put("*",74);
+//		TablaDeSimbolos.put("+",75);
+//		TablaDeSimbolos.put(",",76);
+//		TablaDeSimbolos.put("-",77);
+//		TablaDeSimbolos.put("/",79);
+//		TablaDeSimbolos.put(";",91);
+//		TablaDeSimbolos.put("=",93);
+//		TablaDeSimbolos.put("{",123);
+//		TablaDeSimbolos.put("}",125);
+//
+//		//COMPARADORES Y ASIGNACION
+//		TablaDeSimbolos.put(":=", 256);
+//		TablaDeSimbolos.put(">=", 257);
+//		TablaDeSimbolos.put("<=", 258);
+//		TablaDeSimbolos.put("=!", 259);
+//		TablaDeSimbolos.put(">=", 260);
+		
+		//PALABRAS RESERVADAS
+		TablaDeSimbolos.put("if", 261);
+		TablaDeSimbolos.put("then", 262);
+		TablaDeSimbolos.put("else", 263);
+		TablaDeSimbolos.put("end-if", 264);
+		TablaDeSimbolos.put("out", 265);
+		TablaDeSimbolos.put("fun", 266);
+		TablaDeSimbolos.put("return", 267);
+		TablaDeSimbolos.put("break", 268);
+
+	}
+	
+	//ACCIONES SEMANTICAS ATOMICAS/SiMPLES
+	
+	
+	//AS1: LEE EL CARACTER
+	private Integer AS_1(Integer i) {
+		buffer = "";
+		return 0;
+	}
+	
+	//AS2: Inicializa el string 
+	private Integer AS_2(Integer i) {
+		buffer += codigoFuente.get(index);
+		return 0;
+	}
+	
+	//AS3: Concatena los símbolos OK
+	private Integer AS_3(Integer i) {
+		index--;
+		return 0;
+	}
+	
+	//AS4: Devuelve el último carácter leído a la entrada
+	private  Integer AS_4(Integer i) {
+		index++;
+		return 0;
+	}
+	//AS5: Buscar en la TS
+	private static void AS_6() {
+		
+	}
+	
+	//AS6: Devuelve el token 
+	private static void AS_7() {
+		
+	}
+	
+	//AS7: Aumenta el contador de la cantidad de saltos de línea
+	private static void AS_8() {
+		
+	}
+	
+	//AS8: Parsea string a punto flotante y verifica si cumple el rango, en caso que cumpla llama a AS5.
+	private static void AS_9() {
+		
+	}
+	
+	//AS9: Parsea string a entero corto y verifica si cumple el rango, en caso que cumpla llama a AS5.
+	private static void AS_10() {
+		
+	}
+	//AS10: truncar la cadena de caracteres en caso de superar la cantidad permitida. 
+	private static void AS_5() {
+		
+	}
+	
+	//AS10: Toma el simbolo, lo ignora y pasa al siguiente estado para leer el proximo caracter.
+	private static void AS_11() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_12() {
+		
+	}
+	
+	//ACCIONES SEMANTICAS COMPUESTAS
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_13() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_14() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_15() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_16() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_17() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_18() {
+		
+	}	
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_19() {
+		
+	}
+	
+	//AS11: Reconoce un salto de linea, y verifica que el carácter anterior haya sido una “ / “
+	private static void AS_20() {
+		
+	}
+	//MATRIZ DE ACCIONES SEMANTICAS
+	
+	private static int[][] inicializarMatrizDeAccionesSemanticas() {	
+
+		int [][] matrizTransicion = {
+				//LM	lm		DIGITO	BLANCO	/n	TAB	F	_	= 	<	>	(	)	{	}	,	;	-	 + 	/	.	!	 ' 	
+				{2,		2,		13,		0,		7,	0,	2,	0,	2,	2,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	13,	0,	14},	
+				{3,		3,		3,		17,		17,	17,	3,	3,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17,	17},	
+				{4,		4,		4,		4,		4,	4,	4,	4,	15,	16,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4},	
+				{4,		4,		4,		4,		4,	4,	4,	4,	15,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4},	
+				{4,		4,		4,		4,		4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	15,	15},	
+				{0,		0,		0,		0,		7,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},	
+				{18,	18,		3,		18,		18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	18,	3,	18,	18},	
+				{19,	19,		3,		19,		19,	19,	3,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19},	
+				{19,	19,		3,		19,		19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	3,	3,	19,	19,	19,	19},	
+				{19,	19,		3,		19,		19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19,	19},	
+				{3,		3,		3,		3,		0,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	10,	3,	3,	5},	
+				{0,		0,		0,		10,		20,	10,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	10,	0,	0,	0},	
+				{3,		3,		3,		3,		0,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	0,	3,	3,	0}	
+		
+		};
+		
+		return matrizTransicion;
+	}
+	
+	private void cargarAccionesSemanticas() {
+
+			Function<Integer,Integer> aux;
+			
+			aux = this::AS_1;
+			AccionesSemanticas.add(aux);
+
+			aux = this::AS_2;
+			AccionesSemanticas.add(aux);
+			
+			aux = this::AS_3;
+			AccionesSemanticas.add(aux);
+
+			aux = this::AS_4;
+			AccionesSemanticas.add(aux);
+
+	}
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,25 +8,7 @@ import compilador.Lexico;
 
 
 public class Main { 
-	
-	
-	
 
-	
-//	public static ArrayList<String> getPalabras(ArrayList<Character> CodigoFuente) {
-//		ArrayList<String> palabras = new ArrayList<String>();
-//		String palabra = "";
-//		for(char simbolo:CodigoFuente) {
-//			if((simbolo >= 'a' && simbolo <= 'z') || (simbolo >= 'A' && simbolo <= 'Z')) {
-//				palabra += simbolo;
-//			}else {
-//				if(palabra != "")
-//					palabras.add(palabra);		
-//				palabra = "";
-//			}
-//		}
-//		return palabras;
-//	}
     
     public static void main(String[] args) throws IOException {
     	String path = "C:\\Users\\Leandro\\Desktop\\Compiladores\\Compilador2.0\\src\\test\\test.txt";
